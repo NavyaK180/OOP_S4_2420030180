7eade0a104a8ca08a0d87cc1dd004b81c5998d22
NavyaK180 <2420030180@klh.edu.in>
2025-07-29 14:27:38 +0530

    Add files via upload

---
+++ controlstatement/src/homeAssignment/EvenOddNumber.java
@@ -0,0 +1,22 @@
+package homeAssignment;
+
+import java.util.Scanner;
+
+
+public class EvenOddNumber {
+
+	public static void main(String[] args) {
+		// Check if a number is even or odd.
+		System.out.println("Enter a Number");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(a % 2 == 0)
+			System.out.println("The number is even");
+		else
+			System.out.println("The number is odd");
+		
+
+	}
+
+}
---
+++ controlstatement/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module homeAssignment {
+}

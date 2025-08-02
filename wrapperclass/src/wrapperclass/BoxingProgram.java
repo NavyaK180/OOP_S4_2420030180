0b65b78bce7f525bf569f2fb9df7e3835ef687b1
NavyaK180 <2420030180@klh.edu.in>
2025-08-02 14:14:00 +0530

    Add files via upload

--- wrapperclass/src/wrapperclass/BoxingProgram.java
+++ wrapperclass/src/wrapperclass/BoxingProgram.java
@@ -5,7 +5,7 @@ public class BoxingProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//boxing program
-		int a = 10;
+		int a = 14;
         Integer i = new Integer(a);
         System.out.println("Boxed Integer: " + i);
         

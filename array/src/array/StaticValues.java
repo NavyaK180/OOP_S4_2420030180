966aeac3073e7c2bef5d7350a8c3d1d235e1cd67
NavyaK180 <2420030180@klh.edu.in>
2025-08-03 19:28:11 +0530

    Add files via upload

---
+++ array/src/array/StaticValues.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.*;
+public class StaticValues {
+
+	public static void main(String[] args) {
+		// program to print elements with static values
+		
+     int arr[] = {1,4,6};
+    for(int i=0; i<3; i++) {
+    	 System.out.println(arr[i]);
+     }
+  
+}
+}
